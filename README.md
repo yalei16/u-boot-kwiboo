68564d86624fccabdfd25c0706d956d7ad3dc286
ZHANG Yuntian <yt@radxa.com>
2025-01-24 03:50:22 +0000

    chore: release 2025.04-1

---
+++ README.md
@@ -0,0 +1,8 @@
+# u-boot-kwiboo
+
+[![Release](https://github.com/radxa-pkg/u-boot-kwiboo/actions/workflows/release.yml/badge.svg)](https://github.com/radxa-pkg/u-boot-kwiboo/actions/workflows/release.yml)
+
+Experimental U-Boot based on [Kwiboo's fork](https://github.com/Kwiboo/u-boot-rockchip)
+
+As Kwiboo frquently rebases his branches, we do not guarantee the reproducibility
+of this package.
---
+++ devenv.nix
@@ -0,0 +1,36 @@
+{ pkgs, lib, config, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    bash-completion
+    mdbook
+    mdbook-admonish
+    mdbook-cmdrun
+    mdbook-i18n-helpers
+    mdbook-linkcheck
+    mdbook-toc
+    ncurses
+  ];
+
+  pre-commit = {
+    hooks = {
+      commitizen.enable = true;
+      shellcheck = {
+        enable = true;
+        entry = lib.mkForce "${pkgs.shellcheck}/bin/shellcheck -x";
+      };
+      shfmt.enable = true;
+      statix.enable = true;
+      typos = {
+        enable = true;
+        excludes = [
+        ];
+        settings.ignored-words = [
+        ];
+      };
+    };
+  };
+
+  starship.enable = true;
+}
